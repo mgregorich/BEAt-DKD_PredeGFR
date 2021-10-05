82f94c6bae9fa79c219adacd989d66560d8abf24
Michael Kammer <michael.kammer@mailbox.org>
2021-10-05 10:05:03 +0200

    REF: moved initialisation to separate file
    Enables loading setup and initializing global values in all other files without calling 00_main.R.

--- 00_main.R
+++ 00_main.R
@@ -4,30 +4,11 @@
 # Info: Linear mixed effects model for the prediction of eGFR (BEAt-DKD)
 ###################################################
 
-
 rm(list=ls())
 
-pacman::p_load(tidyr, plyr, reshape2, ggplot2, openxlsx, stringr, transplantr, skimr,
-               lme4, readxl, purrr, janitor, tableone, dplyr, splitstackshape,
-               nlme, lmerTest, MuMIn, JMbayes, splines, rms, Hmisc, concreg, caret, MASS, performance,
-               pmsampsize)
- 
+# Initialise global values
 
-# ------ Initialization 
-set.seed(12345)
-
-# Data and Paths
-data.path = "../Data/"
-GCKD.path = paste0(data.path, "GCKD/")
-PROVALID.path = paste0(data.path, "PROVALID/")
-DIACORE.path = paste0(data.path, "DIACORE/")
-
-out.path = "../Output/"
-
-slope_cutpoint=-3
-
-# Load auxiliary functions 
-source("functions_aux.R")
+source("setup.R")
 
 
 # ------- Start the fun
---
+++ setup.R
@@ -0,0 +1,26 @@
+###################################################
+# Date: 30/06/2021
+# Info: Initialisation and global values
+###################################################
+
+pacman::p_load(tidyr, plyr, reshape2, ggplot2, openxlsx, stringr, transplantr, skimr,
+               lme4, readxl, purrr, janitor, tableone, dplyr, splitstackshape,
+               nlme, lmerTest, MuMIn, JMbayes, splines, rms, Hmisc, concreg, caret, MASS, performance,
+               pmsampsize)
+
+
+# ------ Initialization 
+set.seed(12345)
+
+# Data and Paths
+data.path = "../Data/"
+GCKD.path = paste0(data.path, "GCKD/")
+PROVALID.path = paste0(data.path, "PROVALID/")
+DIACORE.path = paste0(data.path, "DIACORE/")
+
+out.path = "../Output/"
+
+slope_cutpoint=-3
+
+# Load auxiliary functions 
+source("functions_aux.R")
